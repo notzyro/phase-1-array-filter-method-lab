3ef62458cb8747e48f5d2953b676a8efa5317df4
Edward Angelo Salas <zyro670@gmail.com>
2022-03-27 14:27:40 -0400

    commit tree 070827b08cb24e06109a96bd42278902d5bc14a8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+const result = drivers.findMatching(drivers => drivers === 'Bobby');
+console.log(result);
+
+
+
+function findMatching(drivers) {   
+    var test = [ ]; 
+    for (const driver of drivers) {
+        if (driver === 'Bobby') {
+            test += driver;
+        }
+    }
+    return "[ " + test + " ]";
+}
+
+
+findMatching(drivers);
